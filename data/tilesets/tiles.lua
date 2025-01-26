90f1f328619aee89ccb991aaac693cb34214eb85
Daniel Knell <contact@danielknell.co.uk>
2025-01-26 21:55:52 +0000

    feat: update tileset and map

--- data/tilesets/tiles.lua
+++ data/tilesets/tiles.lua
@@ -6,12 +6,12 @@ return {
   class = "",
   tilewidth = 32,
   tileheight = 32,
-  spacing = 0,
-  margin = 0,
+  spacing = 1,
+  margin = 1,
   columns = 8,
   image = "../assets/images/tiles.png",
-  imagewidth = 256,
-  imageheight = 256,
+  imagewidth = 265,
+  imageheight = 1057,
   objectalignment = "unspecified",
   tilerendersize = "tile",
   fillmode = "stretch",
@@ -34,11 +34,11 @@ return {
       properties = {},
       colors = {
         {
-          color = { 255, 0, 0 },
+          color = { 170, 121, 66 },
           name = "path",
           class = "",
           probability = 1,
-          tile = 14,
+          tile = 10,
           properties = {}
         },
         {
@@ -46,15 +46,15 @@ return {
           name = "grass",
           class = "",
           probability = 1,
-          tile = 46,
+          tile = 24,
           properties = {}
         },
         {
-          color = { 0, 0, 255 },
+          color = { 0, 253, 255 },
           name = "water",
           class = "",
           probability = 1,
-          tile = 60,
+          tile = 14,
           properties = {}
         }
       },
@@ -76,19 +76,19 @@ return {
           tileid = 3
         },
         {
-          wangid = { 0, 3, 0, 3, 0, 1, 0, 3 },
+          wangid = { 0, 2, 0, 2, 0, 3, 0, 2 },
           tileid = 4
         },
         {
-          wangid = { 0, 1, 0, 1, 0, 3, 0, 3 },
+          wangid = { 0, 3, 0, 3, 0, 2, 0, 2 },
           tileid = 5
         },
         {
-          wangid = { 0, 3, 0, 1, 0, 1, 0, 1 },
+          wangid = { 0, 2, 0, 3, 0, 3, 0, 3 },
           tileid = 6
         },
         {
-          wangid = { 0, 3, 0, 1, 0, 1, 0, 3 },
+          wangid = { 0, 2, 0, 3, 0, 3, 0, 2 },
           tileid = 7
         },
         {
@@ -108,19 +108,19 @@ return {
           tileid = 11
         },
         {
-          wangid = { 0, 3, 0, 1, 0, 3, 0, 1 },
+          wangid = { 0, 2, 0, 3, 0, 2, 0, 3 },
           tileid = 12
         },
         {
-          wangid = { 0, 1, 0, 1, 0, 1, 0, 3 },
+          wangid = { 0, 3, 0, 3, 0, 3, 0, 2 },
           tileid = 13
         },
         {
-          wangid = { 0, 1, 0, 1, 0, 1, 0, 1 },
+          wangid = { 0, 3, 0, 3, 0, 3, 0, 3 },
           tileid = 14
         },
         {
-          wangid = { 0, 1, 0, 3, 0, 1, 0, 1 },
+          wangid = { 0, 3, 0, 2, 0, 3, 0, 3 },
           tileid = 15
         },
         {
@@ -140,19 +140,19 @@ return {
           tileid = 19
         },
         {
-          wangid = { 0, 1, 0, 3, 0, 3, 0, 3 },
+          wangid = { 0, 3, 0, 2, 0, 2, 0, 2 },
           tileid = 20
         },
         {
-          wangid = { 0, 1, 0, 3, 0, 3, 0, 1 },
+          wangid = { 0, 3, 0, 2, 0, 2, 0, 3 },
           tileid = 21
         },
         {
-          wangid = { 0, 1, 0, 1, 0, 3, 0, 1 },
+          wangid = { 0, 3, 0, 3, 0, 2, 0, 3 },
           tileid = 22
         },
         {
-          wangid = { 0, 3, 0, 3, 0, 1, 0, 1 },
+          wangid = { 0, 2, 0, 2, 0, 3, 0, 3 },
           tileid = 23
         },
         {
@@ -172,88 +172,232 @@ return {
           tileid = 27
         },
         {
-          wangid = { 0, 3, 0, 3, 0, 3, 0, 3 },
+          wangid = { 0, 2, 0, 2, 0, 2, 0, 2 },
           tileid = 28
         },
         {
-          wangid = { 0, 3, 0, 1, 0, 3, 0, 3 },
+          wangid = { 0, 2, 0, 3, 0, 2, 0, 2 },
           tileid = 29
         },
         {
-          wangid = { 0, 1, 0, 3, 0, 1, 0, 3 },
+          wangid = { 0, 3, 0, 2, 0, 3, 0, 2 },
           tileid = 30
         },
         {
-          wangid = { 0, 3, 0, 3, 0, 3, 0, 1 },
+          wangid = { 0, 2, 0, 2, 0, 2, 0, 3 },
           tileid = 31
         },
         {
-          wangid = { 0, 3, 0, 3, 0, 2, 0, 3 },
+          wangid = { 0, 3, 0, 1, 0, 2, 0, 3 },
+          tileid = 32
+        },
+        {
+          wangid = { 0, 3, 0, 2, 0, 1, 0, 3 },
+          tileid = 33
+        },
+        {
+          wangid = { 0, 3, 0, 2, 0, 3, 0, 1 },
+          tileid = 34
+        },
+        {
+          wangid = { 0, 1, 0, 3, 0, 2, 0, 3 },
+          tileid = 35
+        },
+        {
+          wangid = { 0, 1, 0, 1, 0, 3, 0, 1 },
           tileid = 36
         },
         {
-          wangid = { 0, 2, 0, 2, 0, 3, 0, 3 },
+          wangid = { 0, 3, 0, 3, 0, 1, 0, 1 },
           tileid = 37
         },
         {
-          wangid = { 0, 3, 0, 2, 0, 2, 0, 2 },
+          wangid = { 0, 1, 0, 3, 0, 3, 0, 3 },
           tileid = 38
         },
         {
-          wangid = { 0, 3, 0, 2, 0, 2, 0, 3 },
+          wangid = { 0, 1, 0, 3, 0, 3, 0, 1 },
           tileid = 39
         },
         {
-          wangid = { 0, 3, 0, 2, 0, 3, 0, 2 },
+          wangid = { 0, 1, 0, 3, 0, 3, 0, 2 },
+          tileid = 40
+        },
+        {
+          wangid = { 0, 2, 0, 3, 0, 3, 0, 1 },
+          tileid = 41
+        },
+        {
+          wangid = { 0, 2, 0, 3, 0, 1, 0, 3 },
+          tileid = 42
+        },
+        {
+          wangid = { 0, 3, 0, 1, 0, 3, 0, 2 },
+          tileid = 43
+        },
+        {
+          wangid = { 0, 1, 0, 3, 0, 1, 0, 3 },
           tileid = 44
         },
         {
-          wangid = { 0, 2, 0, 2, 0, 2, 0, 3 },
+          wangid = { 0, 3, 0, 3, 0, 3, 0, 1 },
           tileid = 45
         },
         {
-          wangid = { 0, 2, 0, 2, 0, 2, 0, 2 },
+          wangid = { 0, 3, 0, 3, 0, 3, 0, 3 },
           tileid = 46
         },
         {
-          wangid = { 0, 2, 0, 3, 0, 2, 0, 2 },
+          wangid = { 0, 3, 0, 1, 0, 3, 0, 3 },
           tileid = 47
         },
         {
-          wangid = { 0, 2, 0, 3, 0, 3, 0, 3 },
+          wangid = { 0, 2, 0, 1, 0, 3, 0, 3 },
+          tileid = 48
+        },
+        {
+          wangid = { 0, 3, 0, 3, 0, 1, 0, 2 },
+          tileid = 49
+        },
+        {
+          wangid = { 0, 3, 0, 3, 0, 0, 0, 1 },
+          tileid = 50
+        },
+        {
+          wangid = { 0, 1, 0, 2, 0, 3, 0, 3 },
+          tileid = 51
+        },
+        {
+          wangid = { 0, 3, 0, 1, 0, 1, 0, 1 },
           tileid = 52
         },
         {
-          wangid = { 0, 2, 0, 3, 0, 3, 0, 2 },
+          wangid = { 0, 3, 0, 1, 0, 1, 0, 3 },
           tileid = 53
         },
         {
-          wangid = { 0, 2, 0, 2, 0, 3, 0, 2 },
+          wangid = { 0, 3, 0, 3, 0, 1, 0, 3 },
           tileid = 54
         },
         {
-          wangid = { 0, 3, 0, 3, 0, 2, 0, 2 },
+          wangid = { 0, 1, 0, 1, 0, 3, 0, 3 },
           tileid = 55
         },
         {
-          wangid = { 0, 3, 0, 3, 0, 3, 0, 3 },
+          wangid = { 0, 3, 0, 2, 0, 2, 0, 1 },
+          tileid = 56
+        },
+        {
+          wangid = { 0, 1, 0, 2, 0, 2, 0, 3 },
+          tileid = 57
+        },
+        {
+          wangid = { 0, 2, 0, 3, 0, 2, 0, 1 },
+          tileid = 58
+        },
+        {
+          wangid = { 0, 1, 0, 2, 0, 3, 0, 2 },
+          tileid = 59
+        },
+        {
+          wangid = { 0, 1, 0, 1, 0, 1, 0, 1 },
           tileid = 60
         },
         {
-          wangid = { 0, 3, 0, 2, 0, 3, 0, 3 },
+          wangid = { 0, 1, 0, 3, 0, 1, 0, 1 },
           tileid = 61
         },
         {
-          wangid = { 0, 2, 0, 3, 0, 2, 0, 3 },
+          wangid = { 0, 3, 0, 1, 0, 3, 0, 1 },
           tileid = 62
         },
         {
-          wangid = { 0, 3, 0, 3, 0, 3, 0, 2 },
+          wangid = { 0, 1, 0, 1, 0, 1, 0, 3 },
           tileid = 63
+        },
+        {
+          wangid = { 0, 2, 0, 3, 0, 1, 0, 2 },
+          tileid = 64
+        },
+        {
+          wangid = { 0, 2, 0, 1, 0, 3, 0, 2 },
+          tileid = 65
+        },
+        {
+          wangid = { 0, 3, 0, 2, 0, 1, 0, 2 },
+          tileid = 66
+        },
+        {
+          wangid = { 0, 2, 0, 1, 0, 2, 0, 3 },
+          tileid = 67
+        },
+        {
+          wangid = { 0, 3, 0, 1, 0, 1, 0, 2 },
+          tileid = 68
+        },
+        {
+          wangid = { 0, 2, 0, 1, 0, 1, 0, 3 },
+          tileid = 69
+        },
+        {
+          wangid = { 0, 1, 0, 3, 0, 1, 0, 2 },
+          tileid = 70
+        },
+        {
+          wangid = { 0, 2, 0, 1, 0, 3, 0, 1 },
+          tileid = 71
+        },
+        {
+          wangid = { 0, 2, 0, 2, 0, 1, 0, 3 },
+          tileid = 72
+        },
+        {
+          wangid = { 0, 3, 0, 1, 0, 2, 0, 2 },
+          tileid = 73
+        },
+        {
+          wangid = { 0, 2, 0, 2, 0, 3, 0, 1 },
+          tileid = 74
+        },
+        {
+          wangid = { 0, 1, 0, 3, 0, 2, 0, 2 },
+          tileid = 75
+        },
+        {
+          wangid = { 0, 1, 0, 3, 0, 2, 0, 1 },
+          tileid = 76
+        },
+        {
+          wangid = { 0, 1, 0, 2, 0, 3, 0, 1 },
+          tileid = 77
+        },
+        {
+          wangid = { 0, 3, 0, 1, 0, 2, 0, 1 },
+          tileid = 78
+        },
+        {
+          wangid = { 0, 1, 0, 2, 0, 1, 0, 3 },
+          tileid = 79
+        },
+        {
+          wangid = { 0, 1, 0, 1, 0, 2, 0, 3 },
+          tileid = 84
+        },
+        {
+          wangid = { 0, 3, 0, 2, 0, 1, 0, 1 },
+          tileid = 85
+        },
+        {
+          wangid = { 0, 1, 0, 1, 0, 3, 0, 2 },
+          tileid = 86
+        },
+        {
+          wangid = { 0, 2, 0, 3, 0, 1, 0, 1 },
+          tileid = 87
         }
       }
     }
   },
-  tilecount = 64,
+  tilecount = 264,
   tiles = {}
 }
